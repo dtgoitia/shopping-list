69317ffc17bfab31b86eec34e8211accb6156b1e
David Torralba Goitia <david.torralba.goitia@gmail.com>
2022-01-22 10:51:17 +0000

    Add set-are-equal operation

--- webapp/src/set.test.ts
+++ webapp/src/set.test.ts
@@ -1,4 +1,4 @@
-import { setBContainsSetA } from "./set";
+import { areEqual, setBContainsSetA } from "./set";
 
 describe("Set operations", () => {
   test("set A is contained inside set B", () => {
@@ -11,5 +11,23 @@ describe("Set operations", () => {
     const a = new Set([1, 2, 3, 3]);
     const b = new Set([1, 2]);
     expect(setBContainsSetA(a, b)).toBe(false);
+  });
+
+  test("set A equals set B", () => {
+    const a = new Set([1, 3]);
+    const b = new Set([3, 1]);
+    expect(areEqual(a, b)).toBe(true);
+  });
+
+  test("set A does not equal set B - different size", () => {
+    const a = new Set([1, 3]);
+    const b = new Set([1, 2, 3]);
+    expect(areEqual(a, b)).toBe(false);
+  });
+
+  test("set A does not equal set B - same size", () => {
+    const a = new Set([1, 3]);
+    const b = new Set([1, 2]);
+    expect(areEqual(a, b)).toBe(false);
   });
 });
--- webapp/src/set.ts
+++ webapp/src/set.ts
@@ -1,3 +1,15 @@
+export function areEqual<T>(a: Set<T>, b: Set<T>): boolean {
+  if (a.size !== b.size) return false;
+
+  for (const item of a) {
+    if (!b.has(item)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 export function setBContainsSetA<T>(setA: Set<T>, setB: Set<T>): boolean {
   const reminder = new Set(setA);
   for (const itemA of setA) {
