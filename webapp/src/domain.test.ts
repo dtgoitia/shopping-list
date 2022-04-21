1d99c2f3722baef6f5d9802e197dc2848721a787
David Torralba Goitia <david.torralba.goitia@gmail.com>
2022-04-21 08:38:59 +0100

    Implement item autocompletion logic

---
+++ webapp/src/domain.test.ts
@@ -0,0 +1,45 @@
+import { Item, ItemAutocompleter } from "./domain";
+
+describe("Find items", () => {
+  const defaultArgs = { shop: "Lidl", toBuy: true };
+
+  const coder: Item = { id: 1, name: "Coder", ...defaultArgs };
+  const code: Item = { id: 2, name: "Code", ...defaultArgs };
+  const cocoa: Item = { id: 3, name: "Cocoa", ...defaultArgs };
+  const banana: Item = { id: 4, name: "Banana", ...defaultArgs };
+
+  const items: Item[] = [coder, code, cocoa, banana];
+
+  const completer = new ItemAutocompleter(items);
+
+  test("by prefix", () => {
+    const matched = completer.search(["co"]);
+    expect(matched).toEqual(new Set([coder, code, cocoa]));
+  });
+
+  test("ignores case", () => {
+    const uppercaseMatch = completer.search(["CO"]);
+    expect(uppercaseMatch).toEqual(new Set([coder, code, cocoa]));
+
+    const lowercaseMatch = completer.search(["co"]);
+    expect(lowercaseMatch).toEqual(new Set([coder, code, cocoa]));
+  });
+
+  test("match the start of any word in the item", () => {
+    const bigCocoa: Item = { id: 5, name: "Big cocoa", ...defaultArgs };
+    const items: Item[] = [coder, bigCocoa, banana];
+
+    const completer = new ItemAutocompleter(items);
+
+    const matched = completer.search(["co"]);
+    expect(matched).toEqual(new Set([coder, bigCocoa]));
+  });
+
+  test("match multiple prefixes", () => {
+    // let the UI split by space and pass each chunk to the autocompleter
+    const prefixes: string[] = ["cod", "ban"];
+
+    const matched = completer.search(prefixes);
+    expect(matched).toEqual(new Set([coder, code, banana]));
+  });
+});
--- webapp/src/domain.ts
+++ webapp/src/domain.ts
@@ -1,3 +1,4 @@
+import { buildTrie, findWords, TrieNode, Word } from "./autocomplete";
 import { ITEMS, LOAD_ITEMS_FROM_CONFIG } from "./config";
 import storage from "./localStorage";
 
@@ -58,3 +59,78 @@ export function addItem(items: Item[], name: ItemName, shop: ShopName): Item[] {
 // items <--- source of truth
 // toBuy <-- derived from 'items'
 // inventory <-- derived from 'items'
+
+interface WordsToItemMap {
+  [w: Word]: Item[];
+}
+export class ItemAutocompleter {
+  private trie: TrieNode;
+  private wordToItems: WordsToItemMap;
+  constructor(private readonly items: Item[]) {
+    const [words, map] = this.itemsToWords(items);
+    this.trie = buildTrie(words);
+    this.wordToItems = map;
+  }
+
+  public search(prefixes: string[]): Set<Item> {
+    const results: Set<Item> = new Set();
+
+    prefixes
+      .map((prefix) => this.searchSinglePrefix(prefix))
+      .map((items) => [...items])
+      .flat()
+      .forEach((item) => results.add(item));
+
+    return results;
+  }
+
+  private searchSinglePrefix(prefix: string): Set<Item> {
+    const words = findWords(this.trie, prefix.toLowerCase());
+    const items = this.getItemsFromWords(words);
+    return items;
+  }
+
+  private itemsToWords(items: Item[]): [Word[], WordsToItemMap] {
+    const words: Set<Word> = new Set();
+    const map: WordsToItemMap = {};
+
+    for (const item of items) {
+      const itemWords = this.getWordsFromItem(item);
+
+      for (const word of itemWords) {
+        words.add(word);
+
+        if (!map[word]) {
+          map[word] = [item];
+        } else {
+          map[word].push(item);
+        }
+      }
+    }
+
+    const wordList: Word[] = [...words];
+
+    return [wordList, map];
+  }
+
+  private getWordsFromItem(item: Item): Set<Word> {
+    const itemWords = [item.name]
+      .map((name) => name.toLowerCase())
+      .map((name) => name.split(" "))
+      .flat();
+
+    const words = new Set(itemWords);
+    return words;
+  }
+
+  private getItemsFromWords(words: Set<string>): Set<Item> {
+    const items: Set<Item> = new Set();
+
+    for (const word of words) {
+      const wordItems = this.wordToItems[word];
+      wordItems.forEach((word) => items.add(word));
+    }
+
+    return items;
+  }
+}
